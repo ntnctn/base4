07cc8c7d25cf0118ba362c4e8798522dd852df2e
ntnctn <nikkattie@gmail.com>
2025-03-06 16:46:43 +0300

    base3copy

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname))); // Обслуживание статических файлов
+
+// Получение списка товаров
+app.get('/api/products', (req, res) => {
+    fs.readFile(path.join(__dirname, 'products.json'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Ошибка чтения файла');
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+// Обработчик корневого пути для пользовательского интерфейса
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
+
